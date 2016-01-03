f0d6955b49b711ba423252a30876537e18bcd7b6
acid <acid@yandex-team.ru>
2016-01-03 21:24:42 +0300

    Move ball with gesture

--- core/src/com/mygdx/game/Ball.java
+++ core/src/com/mygdx/game/Ball.java
@@ -1,15 +1,27 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 
 public class Ball extends Actor {
     private Sprite boulderSprite;
 
     public Ball(TrashRubbishGame game) {
         boulderSprite = new Sprite(game.getAssetManager().get("boulder.png", Texture.class));
+        setSize(boulderSprite.getWidth(), boulderSprite.getHeight());
+
+        addListener(new ActorGestureListener() {
+            @Override
+            public void pan(InputEvent event, float x, float y, float deltaX, float deltaY) {
+                setX(getX() + deltaX);
+                setY(getY() + deltaY);
+            }
+        });
     }
 
     @Override
--- core/src/com/mygdx/screen/GameScreen.java
+++ core/src/com/mygdx/screen/GameScreen.java
@@ -1,5 +1,8 @@
 package com.mygdx.screen;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.mygdx.game.Ball;
 import com.mygdx.game.TrashRubbishGame;
 
@@ -11,7 +14,9 @@ public class GameScreen extends BasicScreen {
     @Override
     public void show() {
         super.show();
+
         Ball ball = new Ball(game);
+
         stage.addActor(ball);
     }
 
